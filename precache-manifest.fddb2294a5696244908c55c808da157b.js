a6a401e253b1384695b901d15712c667d8fd1ef4
Wesley Paulo <wesley_paulo00@hotmail.com>
2021-05-18 21:19:34 -0300

    Updates

---
+++ precache-manifest.fddb2294a5696244908c55c808da157b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37b90d0944b2a76f2ecdb3343ed2d84",
+    "url": "/GigaWallet/index.html"
+  },
+  {
+    "revision": "73400bdaf6fcdba16848",
+    "url": "/GigaWallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b278fcd294ca3638dc34",
+    "url": "/GigaWallet/static/js/2.f2d541cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GigaWallet/static/js/2.f2d541cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73400bdaf6fcdba16848",
+    "url": "/GigaWallet/static/js/main.f21a9b73.chunk.js"
+  },
+  {
+    "revision": "284b8dcd49a12da37f30",
+    "url": "/GigaWallet/static/js/runtime-main.1cee3fa6.js"
+  },
+  {
+    "revision": "0cba03a87bf632cdf3804c2efd8133ec",
+    "url": "/GigaWallet/static/media/background.0cba03a8.png"
+  },
+  {
+    "revision": "81ffb3c307050b4819d2ee97bf25d62e",
+    "url": "/GigaWallet/static/media/logo.81ffb3c3.png"
+  }
+]);
